e3692f85350ec1e23926524652f8d62b61e12327
Gayane Ghazaryan <74008543+gayaneghazaryan@users.noreply.github.com>
2022-08-22 20:20:06 +0400

    Delete quality_check.sh

--- scripts/quality_check.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-path=$1
-cd $path
-for file in `ls $path | grep "directcDNA"`
-do
-        SAMPLE=`ls $file`
-        echo $SAMPLE
-##      fastqc -t 5 $file/$SAMPLE -o /data/home/arpine_grigoryan/directcDNA
-done
