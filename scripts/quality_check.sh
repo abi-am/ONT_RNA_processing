b0cc0e26d7799c3a81049692de19406ad3c3cdc1
Arpine Grigoryan <111293540+arpin-g@users.noreply.github.com>
2022-10-28 13:44:44 +0400

    Update quality_check.sh

--- scripts/quality_check.sh
+++ scripts/quality_check.sh
@@ -4,6 +4,5 @@ cd $path
 for file in `ls $path | grep "directcDNA"`
 do
         SAMPLE=`ls $file`
-        echo $SAMPLE
-##      fastqc -t 5 $file/$SAMPLE -o /data/home/arpine_grigoryan/directcDNA
+        fastqc -t 5 $file/$SAMPLE -o /data/home/arpine_grigoryan/directcDNA
 done
